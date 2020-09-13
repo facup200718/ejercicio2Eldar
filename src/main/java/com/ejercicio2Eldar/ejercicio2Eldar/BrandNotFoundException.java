37a3ec21fac5093bff181440807b4108fec5b826
Facundo Perez <facup200718@gmail.com>
2020-09-12 21:38:29 -0300

    Cambiada una parte de un sout

--- src/main/java/com/ejercicio2Eldar/ejercicio2Eldar/BrandNotFoundException.java
+++ src/main/java/com/ejercicio2Eldar/ejercicio2Eldar/BrandNotFoundException.java
@@ -3,6 +3,6 @@ package com.ejercicio2Eldar.ejercicio2Eldar;
 public class BrandNotFoundException extends RuntimeException {
 
     BrandNotFoundException(Brand brand) {
-        super("Could not find employee " + brand);
+        super("Could not find brand " + brand);
     }
 }
