747ab2def925e2d5e383884baa23a3291651f234
Facundo Perez <facup200718@gmail.com>
2020-09-13 21:09:32 -0300

    Tratando de poner en cloud

--- src/main/java/com/ejercicio2Eldar/ejercicio2Eldar/Ejercicio2EldarApplication.java
+++ src/main/java/com/ejercicio2Eldar/ejercicio2Eldar/Ejercicio2EldarApplication.java
@@ -3,11 +3,13 @@ package com.ejercicio2Eldar.ejercicio2Eldar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-
+@RestController
 public class Ejercicio2EldarApplication {
+
 	public static void main( String[] args ) {
 		SpringApplication.run(Ejercicio2EldarApplication.class, args);
 	}
